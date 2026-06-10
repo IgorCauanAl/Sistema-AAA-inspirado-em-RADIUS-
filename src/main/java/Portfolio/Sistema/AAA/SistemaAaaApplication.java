df93f1db0db29fba2d79800a35aee3d3d2d328eb
IgorCauanAl <igorcauawo@gmail.com>
2026-06-10 15:21:49 -0300

    Envio das configurações iniciais

---
+++ src/main/java/Portfolio/Sistema/AAA/SistemaAaaApplication.java
@@ -0,0 +1,13 @@
+package Portfolio.Sistema.AAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Portfolio/Sistema/AAA/SistemaAaaApplicationTests.java
@@ -0,0 +1,13 @@
+package Portfolio.Sistema.AAA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
